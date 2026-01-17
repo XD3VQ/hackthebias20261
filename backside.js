58a59bc04be1d975d9b3c2484a16a4b05290125f
reverse-dev <svenk964@mtroyal.ca>
2026-01-17 05:31:00 -0700

    I updated teh basic html file with some work later nicer looking ui and basic recuirter and no student login status rn

---
+++ backside.js
@@ -0,0 +1,9 @@
+//recuriter stuff first intiallizing the buttons easier
+document.getElementById("recruiter").addEventListener("click", () => {
+    location.href = "recruiter_login.html";
+});
+
+//student stuff
+document.getElementById("student").addEventListener("click", () => {
+    location.href = "student_login.html";
+});
